c974693ae7a4691a8eb5be554e8fea20da7e363b
prastyon <50089083+prastyon@users.noreply.github.com>
2022-06-22 01:21:53 +0700

    perbaikan update bukti

--- server/models/buktiModel.js
+++ server/models/buktiModel.js
@@ -45,8 +45,8 @@ bukti.delete = (id) => {
 
 bukti.update = (id_bukti, bukti) => {
     return new Promise((resolve, reject) => {
-        pool.query(`UPDATE bukti set nama_petugas = ?, foto_sekolah = ?, jam_sampai = ?, jam_selesai = ?, foto_bukti = ?, nama_guru = ?, catatan = ?, kontak_sekolah = ?, deskripsi_kendala = ?, foto_kendala = ? WHERE id_bukti = ?`,
-                    [bukti.nama_petugas, bukti.foto_sekolah, bukti.jam_sampai, bukti.jam_selesai,bukti.foto_bukti, bukti.nama_guru, bukti.catatan, bukti.kontak_sekolah, bukti.deskripsi_kendala, bukti.foto_kendala],
+        pool.query(`UPDATE bukti set nama_petugas = ?, foto_sekolah = ?, nama_sekolah = ?, jam_sampai = ?, jam_selesai = ?, foto_bukti = ?, nama_guru = ?, catatan = ?, kontak_sekolah = ?, deskripsi_kendala = ?, foto_kendala = ? WHERE id_bukti = ?`,
+                    [bukti.nama_petugas, bukti.foto_sekolah, bukti.nama_sekolah, bukti.jam_sampai, bukti.jam_selesai,bukti.foto_bukti, bukti.nama_guru, bukti.catatan, bukti.kontak_sekolah, bukti.deskripsi_kendala, bukti.foto_kendala, id_bukti],
             (err, results) => {
             if (err) {
                 return reject(err)
